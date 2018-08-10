901c23c573ae342d8d554484ca80d44163c30870
Kristijan Sedlak <xpepermint@gmail.com>
2018-08-11 01:15:02 +0200

    Format cli sandbox output

--- packages/specron-cli/src/commands/sandbox.ts
+++ packages/specron-cli/src/commands/sandbox.ts
@@ -1,15 +1,26 @@
 import { Sandbox } from '@specron/sandbox';
+import { Printer } from '@hayspec/reporter';
 
 /**
  * Starts Ethereum sandbox server.
  */
 export default async function (argv) {
   const { port, host, ttl } = argv;
+  const printer = new Printer();
 
   const sandbox = new Sandbox();
   try {
     await sandbox.listen(port, host);
-    console.log(`Listening at ${host}:${port} ...`);
+    printer.end();
+    printer.end(
+      printer.indent(1, ''),
+      'Sandbox blockchain'
+    );
+    printer.end(
+      printer.indent(2, ''),
+      printer.colorize('gray', `Listening at ${host}:${port} ...`)
+    );
+    printer.end();
   } catch (e) {
     console.error(e);
   }
