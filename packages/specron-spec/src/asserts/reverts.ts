900191a3846d40cbf9632975c165407a278d2529
Tadej Vengust <vengust.tadej@gmail.com>
2018-09-13 11:53:54 +0200

    Add reason check to reverts.

--- packages/specron-spec/src/asserts/reverts.ts
+++ packages/specron-spec/src/asserts/reverts.ts
@@ -1,22 +1,26 @@
 /**
  * 
  */
-function isRevertError(e) {
-  return e === 'revert' || e.message.search('revert') >= 0;
+function isRevertError(e, message?: string) {
+  const isRevert = String(e).search('revert') >= 0 || (e.message !== undefined && e.message.search('revert') >= 0);
+  if(isRevert && message !== undefined) {
+    return String(e).search(message) >= 0 || (e.message !== undefined && e.message.search(message) >= 0);
+  }
+  return isRevert;
 }
 
 /**
  * 
  */
-export default function reverts (fn: () => any) {
+export default function reverts (fn: () => any, message?: string) {
   try {
     const res = fn();
     if (res instanceof Promise) {
-      return res.then(() => false).catch((e) => isRevertError(e));
+      return res.then(() => false).catch((e) => isRevertError(e, message));
     } else {
       return false;
     }
   } catch (e) {
-    return isRevertError(e);
+    return isRevertError(e, message);
   }
 }
