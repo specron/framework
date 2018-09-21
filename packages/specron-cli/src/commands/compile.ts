1b2b0e4d2fc776cd2f77c49b72fa3832c585cc93
Tadej Vengust <vengust.tadej@gmail.com>
2018-09-21 10:07:15 +0200

    Change cli to accept array of log severities for compiler output.

--- packages/specron-cli/src/commands/compile.ts
+++ packages/specron-cli/src/commands/compile.ts
@@ -1,14 +1,10 @@
-import { Compiler, DefaultReporter, Severity } from '@specron/compiler';
+import { Compiler, DefaultReporter } from '@specron/compiler';
 
 /**
  * Compiles solidity contracts.
  */
 export default async function (argv) {
-  const { match, build, warnings } = argv;
-  const severities : Severity[] = ['error'];
-  if(warnings === undefined || warnings) {
-    severities.push('warning');
-  }
+  const { match, build, severities } = argv;
 
   const compiler = new Compiler({
     reporter: new DefaultReporter(severities),
