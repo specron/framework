cf9f24969b4029b8b2f5277498c6879091e49302
Kristijan Sedlak <xpepermint@gmail.com>
2018-09-26 19:54:25 +0200

    Fix sendbox methods, move solc to peer dependencies

--- packages/specron-sandbox/src/core/sandbox.ts
+++ packages/specron-sandbox/src/core/sandbox.ts
@@ -10,20 +10,26 @@ export class Sandbox {
    * Starts the server.
    */
   public async listen(port = 8545, host = '127.0.0.1') {
+
     await new Promise((resolve, reject) => {
       this.server = ganache.server();
       this.server.listen(port, host, (e) => e ? reject(e) : resolve());
     });
+
+    return this;
   }
 
   /**
    * Stops the server.
    */
   public async close () {
+
     if (this.server) {
       this.server.close();
       this.server = null;
     }
+
+    return this;
   }
 
 }
